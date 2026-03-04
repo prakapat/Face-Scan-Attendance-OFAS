ad00357d2d5006f31eb473367d40ed3f3b4a8572
prakapat <150698745+prakapat@users.noreply.github.com>
2026-03-04 16:51:29 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxPe4zYx1QFTiBZ31Fq6S5v7TSUg52q0s4qH6sV94aVSSODEUawasEtjSRtxraLuMU3/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwSJgeKD_bqrX7-L95h1nEYUWK0pHv63Ur4dwymlSooRjgFTIeCj8EyYuCk904ocE-7/exec';
